97bfc452e574b4bb94630faa5a7c95ab1f724867
tovi <contactovictorgamboa@gmail.com>
2020-06-28 14:04:27 -0500

    "Modifacion View desde codigo java"

--- app/src/main/java/co/tovigamboa/primerapp/MainActivity.java
+++ app/src/main/java/co/tovigamboa/primerapp/MainActivity.java
@@ -3,12 +3,16 @@ package co.tovigamboa.primerapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		//setContentView(R.layout.activity_main);
+		TextView miTexto = new TextView(getApplicationContext());
+		miTexto.setText("Mensaje cambiado en el TextView desde codigo java");
+		setContentView(miTexto);
 	}
 }
